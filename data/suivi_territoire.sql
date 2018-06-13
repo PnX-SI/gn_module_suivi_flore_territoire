ff08436bbba15a0b292dce406f200f4275a977bb
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-13 11:46:20 +0200

    script sql d'insertion data

--- data/suivi_territoire.sql
+++ data/suivi_territoire.sql
@@ -38,7 +38,9 @@ CREATE TABLE cor_visit_perturbation (
 COMMENT ON TABLE pr_monitoring_flora_territory.cor_visit_perturbation IS 'Extension de t_base_visit de gn_monitoring, enregistrer les perturbations constatées lors d\une visite';
 
 
-
+CREATE TABLE zp_import(
+    id integer
+)
 
 ---------------
 --PRIMARY KEY--
@@ -87,5 +89,50 @@ ALTER TABLE ONLY cor_visit_perturbation
 -- + Un trigger pour vérifier si id_nomenclature_perturbation dans la table cor_visit_perturbation 
 --   correspond bien à celui stocké dans t_nomenclatures. 
 
+--------
+--DATA--
+--------
+
+INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_default, label_fr, definition_fr )
+VALUES (116, 'ZP', 'Zone de prospection', 'Zone de prospection - suivi flore territoire', 'Zone de prospection',  'Zone de prospection issu du module suivi flore territoire');
+
+-- PROVISOIRE A ADAPTER GRACE AU SHAPEFILE DU CBNA
+INSERT INTO gn_monitoring.t_base_sites
+(id_inventor, id_digitiser, id_nomenclature_type_site, base_site_name, base_site_description, base_site_code, first_use_date, geom )
+SELECT 1, 1, 503, 'zp', '', id, '01-01-2018', ST_TRANSFORM(ST_SetSRID(geom, 2154), 4326)
+FROM pr_monitoring_flora_territory.zp_tmp;
+
+INSERT INTO pr_monitoring_flora_territory.infos_site (id_base_site, cd_nom)
+SELECT id_base_site, zp.cd_nom
+FROM gn_monitoring.t_base_sites bs
+JOIN pr_monitoring_flora_territory.zp_tmp zp ON zp.id::character varying = bs.base_site_code;
 
 
+INSERT INTO ref_geo.bib_areas_types (id_type, type_name, type_desc)
+VALUES (203, 'Mailles25*25', 'Maille INPN 50*50 redécoupé en 25m');
+
+INSERT INTO ref_geo.l_areas (id_type, area_name, area_code, geom, centroid, source)
+SELECT 203, id, id, geom, ST_CENTROID(geom), 'INPN'
+FROM pr_monitoring_flora_territory.maille_tmp;
+
+INSERT INTO ref_geo.li_grids
+SELECT area_code, id_area, ST_XMin(ST_Extent(geom)), ST_XMax(ST_Extent(geom)), ST_YMin(ST_Extent(geom)),ST_YMax(ST_Extent(geom))
+FROM ref_geo.l_areas
+WHERE id_type=203
+GROUP by area_code, id_area;
+
+
+INSERT INTO gn_monitoring.cor_site_area (id_base_site, id_area)
+SELECT bs.id_base_site, a.id_area 
+FROM ref_geo.l_areas a
+JOIN gn_monitoring.t_base_sites bs ON ST_Within(ST_TRANSFORM(a.geom, 4326), bs.geom)
+WHERE id_type=203;
+
+-- TODO Mettre en paramètre l'id du module
+INSERT INTO gn_monitoring.cor_site_application
+SELECT  bs.id_base_site, 19
+FROM gn_monitoring.t_base_sites bs
+JOIN pr_monitoring_flora_territory.zp_tmp zp ON bs.base_site_code  = zp.id::character varying;
+
+DROP TABLE pr_monitoring_flora_territory.zp_tmp;
+DROP TABLE pr_monitoring_flora_territory.maille_tmp;
--- install_db.sh
+++ install_db.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+
+. config/settings.ini
+
+cp data/suivi_terrritoire.sql /tmp/suivi_terrritoire.sql
+
+sudo sed -i "s/MY_ID_MODULE/$MY_ID_MODULE/g" /tmp/suivi_terrritoire
+
+sudo -n -u postgres -s shp2pgsql -W "UTF-8" -s 2154 -D -I /tmp/zp4.shp pr_monitoring_flora_territory.zp_tmp | sudo -n -u postgres -s psql -d geonature2db
+
+sudo -n -u postgres -s shp2pgsql -W "UTF-8" -s 2154 -D -I /tmp/maille_25m.shp pr_monitoring_flora_territory.maille_tmp | sudo -n -u postgres -s psql -d geonature2db
+
+
+export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/suivi_terrritoire.sql &>> var/log/install_sft.logs
