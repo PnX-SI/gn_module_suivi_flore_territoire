2db178acc391e7fab8fd714b19ce933e74f5b0b4
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-19 17:55:59 +0200

    ajout route pour récupérer une zp à partir de l'id_base_site

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -18,13 +18,26 @@ def get_sites_zp():
     return FeatureCollection([d.get_geofeature() for d in data])
 
 
-@blueprint.route('/site/<id_zp>', methods=['GET'])
+@blueprint.route('/site', methods=['GET'])
 @json_resp
-def get_ont_zp(id_zp):
+def get_one_zp():
     '''
-    Retourne la liste des ZP
+    Retourne une ZP à partir de l'id_base_site
     '''
-    data = DB.session.query(TInfoSite).get(id_zp)
+    parameters = request.args
+    q = DB.session.query(TInfoSite)
+    if 'id_base_site' in parameters:
+        q = q.filter(TInfoSite.id_base_site == parameters['id_base_site'])
+    data = DB.session.query(TInfoSite).one()
+    return data.as_dict()
+
+@blueprint.route('/site/<id_base_site>', methods=['GET'])
+@json_resp
+def get_one_zp_id(id_info_site):
+    '''
+    Retourne une ZP à partir de l'id_infi_site
+    '''
+    data = DB.session.query(TInfoSite).get(id_info_site)
     return data.as_dict()
 
 
