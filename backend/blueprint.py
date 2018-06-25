f73bf9c7b9f43bf8fca560a0e0e40aca42a0d62f
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-06-25 10:10:08 +0200

    modifie bluesprint et gitignore

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -23,6 +23,8 @@ def get_sites_zp():
 def get_one_zp():
     '''
     Retourne une ZP à partir de l'id_base_site
+    param:
+        id_base_site: integer
     '''
     parameters = request.args
     q = DB.session.query(TInfoSite)
@@ -33,13 +35,13 @@ def get_one_zp():
         return data.as_dict()
     return None
 
-@blueprint.route('/site/<id_base_site>', methods=['GET'])
+@blueprint.route('/site/<id_infos_site>', methods=['GET'])
 @json_resp
-def get_one_zp_id(id_info_site):
+def get_one_zp_id(id_infos_site):
     '''
     Retourne une ZP à partir de l'id_info_site
     '''
-    data = DB.session.query(TInfoSite).get(id_info_site)
+    data = DB.session.query(TInfoSite).get(id_infos_site)
     return data.as_dict()
 
 
