e9c7e0ebfa8e92e28554f1ad5b0f253414c2d805
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-07-18 10:17:27 +0200

    modif gnModule

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -8,34 +8,32 @@ import { HttpClient } from "@angular/common/http";
 import { DataService } from "./services/data.service";
 import { HttpClientModule } from "@angular/common/http";
 import { StoreService } from "./services/store.service";
-import { OcctaxFormService } from '../../../GeoNature/contrib/occtax/frontend/app/occtax-map-form/form/occtax-form.service';
 import { DetailVisitComponent } from "./detail-visit/detail-visit.component";
-import { RajoutVisitComponent } from "./rajout-visit/rajout-visit.component";
 import { ZpMapListComponent } from "./zp-map-list/zp-map-list.component";
 import { ListVisitComponent } from "./list-visit/list-visit.component";
 import { EditVisitComponent } from "./edit-visit/edit-visit.component";
+import { AddVisitComponent } from "./add-visit/add-visit.component";
 
 // my module routing
 const routes: Routes = 
 [{ path: "", component: ZpMapListComponent},
- { path: "afficheVisit/:idSite", component: ListVisitComponent},
- {path: "afficheMaille/:idSite", component: RajoutVisitComponent},
+ { path: "listVisit/:idSite", component: ListVisitComponent},
+ {path: "addVisit/:idSite", component: AddVisitComponent},
  {path: "infoVisit/:idVisit", component: DetailVisitComponent },
  {path: "editVisit/:idVisit", component: EditVisitComponent}
-//  {path: "afficheMaille/:idSite/:idVisit", component: DetailVisitComponent}
 
   
  ];
 
 @NgModule({
-  declarations: [ZpMapListComponent, RajoutVisitComponent, ListVisitComponent, DetailVisitComponent, EditVisitComponent ],
+  declarations: [ZpMapListComponent, AddVisitComponent, ListVisitComponent, DetailVisitComponent, EditVisitComponent ],
   imports: [
     GN2CommonModule,
     RouterModule.forChild(routes),
     HttpClientModule,
     CommonModule
   ],
-  providers: [HttpClient, DataService, StoreService, OcctaxFormService],
+  providers: [HttpClient, DataService, StoreService],
   bootstrap: []
 })
 
