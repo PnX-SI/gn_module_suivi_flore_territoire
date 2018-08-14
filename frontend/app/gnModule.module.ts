99b24883f8a85c946a692f5e7708abb60260b12b
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-08-14 11:00:08 +0200

    Maj les routes

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -3,39 +3,41 @@ import { GN2CommonModule } from "@geonature_common/GN2Common.module";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { ViewTestComponent } from "./view-test/view-test.component";
-import { TestComponent } from "./test.component";
 import { HttpClient } from "@angular/common/http";
 import { DataService } from "./services/data.service";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HttpClientXsrfModule } from "@angular/common/http";
 import { StoreService } from "./services/store.service";
 import { DetailVisitComponent } from "./detail-visit/detail-visit.component";
 import { ZpMapListComponent } from "./zp-map-list/zp-map-list.component";
 import { ListVisitComponent } from "./list-visit/list-visit.component";
-import { EditVisitComponent } from "./edit-visit/edit-visit.component";
-import { AddVisitComponent } from "./add-visit/add-visit.component";
 import { FormService } from "./services/form.service";
+import { FormVisitComponent } from "./form-visit/form-visit.component";
 
 // my module routing
 const routes: Routes = 
 [{ path: "", component: ZpMapListComponent},
  { path: "listVisit/:idSite", component: ListVisitComponent},
- {path: "addVisit/:idSite", component: AddVisitComponent},
+ {path: "editVisit/:idSite", component: FormVisitComponent},
  {path: "infoVisit/:idVisit", component: DetailVisitComponent },
- {path: "editVisit/:idVisit", component: EditVisitComponent}
+ {path: "editVisit/:idSite/visit/:idVisit", component: FormVisitComponent,}
 
   
  ];
 
 @NgModule({
-  declarations: [ZpMapListComponent, AddVisitComponent, ListVisitComponent, DetailVisitComponent, EditVisitComponent ],
+  declarations: [ZpMapListComponent, ListVisitComponent, DetailVisitComponent, FormVisitComponent,],
   imports: [
     GN2CommonModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'token',
+      headerName: 'token'
+    }),
     RouterModule.forChild(routes),
-    HttpClientModule,
     CommonModule
   ],
   providers: [HttpClient, DataService, StoreService, FormService],
-  bootstrap: []
+  bootstrap: [],
+  
 })
 
 export class GeonatureModule {
