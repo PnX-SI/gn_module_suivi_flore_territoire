1a7bf97f81e45cbe2d16f59f5d82dd848d3036f7
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-08-14 10:29:58 +0200

    fichiers sql pour SFT, séparer structure et data #3

---
+++ data/SFT.sql
@@ -0,0 +1,136 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+CREATE SCHEMA pr_monitoring_flora_territory;
+
+SET search_path = pr_monitoring_flora_territory, pg_catalog;
+
+SET default_with_oids = false;
+
+------------------------
+--TABLES AND SEQUENCES--
+------------------------
+
+CREATE TABLE t_infos_site (
+    id_infos_site serial NOT NULL,
+    id_base_site integer NOT NULL,
+    cd_nom integer NOT NULL  
+);
+COMMENT ON TABLE pr_monitoring_flora_territory.t_infos_site IS 'Extension de t_base_sites de gn_monitoring, permet d\avoir les infos complémentaires d\un site';
+
+
+CREATE TABLE cor_visit_grid (
+    id_area integer NOT NULL,
+    id_base_visit integer NOT NULL,
+    presence boolean NOT NULL,
+    uuid_base_visit UUID DEFAULT public.uuid_generate_v4() 
+    -- rajouter uuid (id unique) pour chaque visite de la table cor_visit_grid --     
+);
+COMMENT ON TABLE pr_monitoring_flora_territory.cor_visit_grid IS 'Enregistrer la présence/absence d\une espèce dans une maille définie lors d\une visite';
+
+
+CREATE TABLE cor_visit_perturbation (
+    id_base_visit integer NOT NULL,
+    id_nomenclature_perturbation integer NOT NULL   
+);
+COMMENT ON TABLE pr_monitoring_flora_territory.cor_visit_perturbation IS 'Extension de t_base_visit de gn_monitoring, enregistrer les perturbations constatées lors d\une visite';
+
+
+---------------
+--PRIMARY KEY--
+---------------
+
+ALTER TABLE ONLY t_infos_site 
+    ADD CONSTRAINT pk_id_t_infos_site PRIMARY KEY (id_infos_site);
+
+ALTER TABLE ONLY cor_visit_grid
+    ADD CONSTRAINT pk_cor_visit_grid PRIMARY KEY (id_area, id_base_visit);
+
+ALTER TABLE ONLY cor_visit_perturbation 
+    ADD CONSTRAINT pk_cor_visit_perturbation PRIMARY KEY (id_base_visit, id_nomenclature_perturbation);
+
+
+
+---------------
+--FOREIGN KEY--
+---------------
+
+ALTER TABLE ONLY t_infos_site 
+    ADD CONSTRAINT fk_infos_site_id_base_site FOREIGN KEY (id_base_site) REFERENCES gn_monitoring.t_base_sites (id_base_site) ON UPDATE CASCADE ON DELETE CASCADE; 
+
+ALTER TABLE ONLY t_infos_site
+    ADD CONSTRAINT fk_infos_site_cd_nom FOREIGN KEY (cd_nom) REFERENCES taxonomie.taxref (cd_nom) ON UPDATE CASCADE;
+
+
+ALTER TABLE ONLY cor_visit_grid
+    ADD CONSTRAINT fk_cor_visit_grid_id_base_visit FOREIGN KEY (id_base_visit) REFERENCES gn_monitoring.t_base_visits (id_base_visit) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY cor_visit_grid
+    ADD CONSTRAINT fk_cor_visit_grid_id_area FOREIGN KEY (id_area) REFERENCES ref_geo.l_areas (id_area);
+
+
+ALTER TABLE ONLY cor_visit_perturbation 
+    ADD CONSTRAINT fk_cor_visit_perturbation_id_base_visit FOREIGN KEY (id_base_visit) REFERENCES gn_monitoring.t_base_visits (id_base_visit) ON UPDATE CASCADE;
+
+ALTER TABLE ONLY cor_visit_perturbation 
+    ADD CONSTRAINT fk_cor_visit_perturbation_id_nomenclature_perturbation FOREIGN KEY (id_nomenclature_perturbation) REFERENCES ref_nomenclatures.t_nomenclatures (id_nomenclature) ON UPDATE CASCADE;
+
+
+
+
+----------------
+-- CREATE VUE--- 
+----------------
+-- to download the data -- 
+
+
+CREATE OR REPLACE VIEW pr_monitoring_flora_territory.export_visits AS 
+WITH
+    observers AS(
+SELECT 
+    v.id_base_visit,
+    string_agg(roles.nom_role::text || ' ' ||  roles.prenom_role::text, ',') AS observateurs
+FROM gn_monitoring.t_base_visits v
+JOIN gn_monitoring.cor_visit_observer observer ON observer.id_base_visit = v.id_base_visit
+JOIN utilisateurs.t_roles roles ON roles.id_role = observer.id_role
+GROUP BY v.id_base_visit
+),
+perturbations AS(
+SELECT 
+    v.id_base_visit,
+    string_agg(n.label_default, ',') AS label_perturbation
+FROM gn_monitoring.t_base_visits v
+JOIN pr_monitoring_flora_territory.cor_visit_perturbation p ON v.id_base_visit = p.id_base_visit
+JOIN ref_nomenclatures.t_nomenclatures n ON p.id_nomenclature_perturbation = n.id_nomenclature
+GROUP BY v.id_base_visit
+)
+-- toutes les mailles d'un site et leur visites
+SELECT sites.id_base_site, cor.id_area, visits.id_base_visit, grid.presence, visits.id_digitiser, visits.visit_date, visits.comments, visits.uuid_base_visit, ar.geom,
+    per.label_perturbation,
+    obs.observateurs,
+    sites.base_site_name,
+    taxon.nom_valide,
+    taxon.cd_nom
+    
+FROM gn_monitoring.t_base_sites sites
+JOIN gn_monitoring.cor_site_area cor ON cor.id_base_site = sites.id_base_site
+JOIN gn_monitoring.t_base_visits visits ON sites.id_base_site = visits.id_base_site
+LEFT JOIN pr_monitoring_flora_territory.cor_visit_grid grid ON grid.id_area = cor.id_area AND grid.id_base_visit = visits.id_base_visit
+JOIN observers obs ON obs.id_base_visit = visits.id_base_visit
+JOIN perturbations per ON per.id_base_visit = visits.id_base_visit
+JOIN pr_monitoring_flora_territory.t_infos_site info ON info.id_base_site = sites.id_base_site
+JOIN taxonomie.taxref taxon ON taxon.cd_nom = info.cd_nom
+JOIN ref_geo.l_areas ar ON ar.id_area = cor.id_area
+
+ORDER BY visits.id_base_visit
+
+------------
+--TRIGGERS--
+------------
+-- Idée: 
+-- + Un trigger pour vérifier si id_nomenclature_perturbation dans la table cor_visit_perturbation 
+--   correspond bien à celui stocké dans t_nomenclatures. 
--- data/SFT_data.sql
+++ data/SFT_data.sql
@@ -5,138 +5,12 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
-CREATE SCHEMA pr_monitoring_flora_territory;
-
 SET search_path = pr_monitoring_flora_territory, pg_catalog;
 
-SET default_with_oids = false;
 
-------------------------
---TABLES AND SEQUENCES--
-------------------------
-
-CREATE TABLE t_infos_site (
-    id_infos_site serial NOT NULL,
-    id_base_site integer NOT NULL,
-    cd_nom integer NOT NULL  
-);
-COMMENT ON TABLE pr_monitoring_flora_territory.t_infos_site IS 'Extension de t_base_sites de gn_monitoring, permet d\avoir les infos complémentaires d\un site';
-
-
-CREATE TABLE cor_visit_grid (
-    id_area integer NOT NULL,
-    id_base_visit integer NOT NULL,
-    presence boolean NOT NULL     
-);
-COMMENT ON TABLE pr_monitoring_flora_territory.cor_visit_grid IS 'Enregistrer la présence/absence d\une espèce dans une maille définie lors d\une visite';
-
-
-CREATE TABLE cor_visit_perturbation (
-    id_base_visit integer NOT NULL,
-    id_nomenclature_perturbation integer NOT NULL   
-);
-COMMENT ON TABLE pr_monitoring_flora_territory.cor_visit_perturbation IS 'Extension de t_base_visit de gn_monitoring, enregistrer les perturbations constatées lors d\une visite';
-
-
----------------
---PRIMARY KEY--
----------------
-
-ALTER TABLE ONLY t_infos_site 
-    ADD CONSTRAINT pk_id_t_infos_site PRIMARY KEY (id_infos_site);
-
-ALTER TABLE ONLY cor_visit_grid
-    ADD CONSTRAINT pk_cor_visit_grid PRIMARY KEY (id_area, id_base_visit);
-
-ALTER TABLE ONLY cor_visit_perturbation 
-    ADD CONSTRAINT pk_cor_visit_perturbation PRIMARY KEY (id_base_visit, id_nomenclature_perturbation);
-
-
-
----------------
---FOREIGN KEY--
----------------
-
-ALTER TABLE ONLY t_infos_site 
-    ADD CONSTRAINT fk_infos_site_id_base_site FOREIGN KEY (id_base_site) REFERENCES gn_monitoring.t_base_sites (id_base_site) ON UPDATE CASCADE ON DELETE CASCADE; 
-
-ALTER TABLE ONLY t_infos_site
-    ADD CONSTRAINT fk_infos_site_cd_nom FOREIGN KEY (cd_nom) REFERENCES taxonomie.taxref (cd_nom) ON UPDATE CASCADE;
-
-
-ALTER TABLE ONLY cor_visit_grid
-    ADD CONSTRAINT fk_cor_visit_grid_id_base_visit FOREIGN KEY (id_base_visit) REFERENCES gn_monitoring.t_base_visits (id_base_visit) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY cor_visit_grid
-    ADD CONSTRAINT fk_cor_visit_grid_id_area FOREIGN KEY (id_area) REFERENCES ref_geo.l_areas (id_area);
-
-
-ALTER TABLE ONLY cor_visit_perturbation 
-    ADD CONSTRAINT fk_cor_visit_perturbation_id_base_visit FOREIGN KEY (id_base_visit) REFERENCES gn_monitoring.t_base_visits (id_base_visit) ON UPDATE CASCADE;
-
-ALTER TABLE ONLY cor_visit_perturbation 
-    ADD CONSTRAINT fk_cor_visit_perturbation_id_nomenclature_perturbation FOREIGN KEY (id_nomenclature_perturbation) REFERENCES ref_nomenclatures.t_nomenclatures (id_nomenclature) ON UPDATE CASCADE;
-
-
-------------
---TRIGGERS--
-------------
--- Idée: 
--- + Un trigger pour vérifier si id_nomenclature_perturbation dans la table cor_visit_perturbation 
---   correspond bien à celui stocké dans t_nomenclatures. 
-
---------
---DATA--
---------
-
-INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_default, label_fr, definition_fr )
-VALUES (ref_nomenclatures.get_id_nomenclature_type('TYPE_SITE'), 'ZP', 'Zone de prospection', 'Zone de prospection - suivi flore territoire', 'Zone de prospection',  'Zone de prospection issu du module suivi flore territoire');
-
--- PROVISOIRE A ADAPTER GRACE AU SHAPEFILE DU CBNA
-INSERT INTO gn_monitoring.t_base_sites
-(id_inventor, id_digitiser, id_nomenclature_type_site, base_site_name, base_site_description, base_site_code, first_use_date, geom )
-SELECT 1, 1, ref_nomenclatures.get_id_nomenclature('TYPE_SITE', 'ZP'), 'zp', '', id, '01-01-2018', ST_TRANSFORM(ST_SetSRID(geom, 2154), 4326)
-FROM pr_monitoring_flora_territory.zp_tmp;
-
-INSERT INTO pr_monitoring_flora_territory.t_infos_site (id_base_site, cd_nom)
-SELECT id_base_site, zp.cd_nom
-FROM gn_monitoring.t_base_sites bs
-JOIN pr_monitoring_flora_territory.zp_tmp zp ON zp.id::character varying = bs.base_site_code;
-
-
-INSERT INTO ref_geo.bib_areas_types (id_type, type_name, type_desc)
-VALUES (203, 'Mailles25*25', 'Maille INPN 50*50 redécoupé en 25m');
-
-INSERT INTO ref_geo.l_areas (id_type, area_name, area_code, geom, centroid, source)
-SELECT 203, id, id, geom, ST_CENTROID(geom), 'INPN'
-FROM pr_monitoring_flora_territory.maille_tmp;
-
-INSERT INTO ref_geo.li_grids
-SELECT area_code, id_area, ST_XMin(ST_Extent(geom)), ST_XMax(ST_Extent(geom)), ST_YMin(ST_Extent(geom)),ST_YMax(ST_Extent(geom))
-FROM ref_geo.l_areas
-WHERE id_type=203
-GROUP by area_code, id_area;
-
-
-INSERT INTO gn_monitoring.cor_site_area (id_base_site, id_area)
-SELECT bs.id_base_site, a.id_area 
-FROM ref_geo.l_areas a
-JOIN gn_monitoring.t_base_sites bs ON ST_Within(ST_TRANSFORM(a.geom, 4326), bs.geom)
-WHERE id_type=203;
-
--- TODO Mettre en paramètre l'id du module
-INSERT INTO gn_monitoring.cor_site_application
-SELECT  bs.id_base_site, MY_ID_MODULE
-FROM gn_monitoring.t_base_sites bs
-JOIN pr_monitoring_flora_territory.zp_tmp zp ON bs.base_site_code  = zp.id::character varying;
-
-DROP TABLE pr_monitoring_flora_territory.zp_tmp;
-DROP TABLE pr_monitoring_flora_territory.maille_tmp;
-
--- Créer la nomenclature des perturbations
+-- to create the nomenclature of perturbations-- 
 INSERT INTO ref_nomenclatures.bib_nomenclatures_types (mnemonique, label_default, definition_default, label_fr, definition_fr)
-VALUES ('TYPE_PERTURBATION', 'Type de perturbations', 'Nomenclature des types de perturbations.', 'Type de perturbations', 'Nomenclatures des types de perturbations.');
-
+    VALUES ('TYPE_PERTURBATION', 'Type de perturbations', 'Nomenclature des types de perturbations.', 'Type de perturbations', 'Nomenclatures des types de perturbations.');
 
 
 INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_default, definition_default, label_fr, definition_fr, id_broader, hierarchy) VALUES 
@@ -210,6 +84,47 @@ INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemoni
 
 
 
+INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_default, label_fr, definition_fr )
+VALUES (ref_nomenclatures.get_id_nomenclature_type('TYPE_SITE'), 'ZP', 'Zone de prospection', 'Zone de prospection - suivi flore territoire', 'Zone de prospection',  'Zone de prospection issu du module suivi flore territoire');
+
+-- PROVISOIRE A ADAPTER GRACE AU SHAPEFILE DU CBNA
+INSERT INTO gn_monitoring.t_base_sites
+(id_inventor, id_digitiser, id_nomenclature_type_site, base_site_name, base_site_description, base_site_code, first_use_date, geom )
+SELECT 1, 1, ref_nomenclatures.get_id_nomenclature('TYPE_SITE', 'ZP'), 'zp', '', id, '01-01-2018', ST_TRANSFORM(ST_SetSRID(geom, 2154), 4326)
+FROM pr_monitoring_flora_territory.zp_tmp;
+
+INSERT INTO pr_monitoring_flora_territory.t_infos_site (id_base_site, cd_nom)
+SELECT id_base_site, zp.cd_nom
+FROM gn_monitoring.t_base_sites bs
+JOIN pr_monitoring_flora_territory.zp_tmp zp ON zp.id::character varying = bs.base_site_code;
+
+--TODO--
+-- parametrer ref_geo.bib_areas_types -- 
+INSERT INTO ref_geo.bib_areas_types (id_type, type_name, type_desc)
+VALUES (203, 'Mailles25*25', 'Maille INPN 50*50 redécoupé en 25m');
+
+INSERT INTO ref_geo.l_areas (id_type, area_name, area_code, geom, centroid, source)
+SELECT 203, id, id, geom, ST_CENTROID(geom), 'INPN'
+FROM pr_monitoring_flora_territory.maille_tmp;
+
+INSERT INTO ref_geo.li_grids
+SELECT area_code, id_area, ST_XMin(ST_Extent(geom)), ST_XMax(ST_Extent(geom)), ST_YMin(ST_Extent(geom)),ST_YMax(ST_Extent(geom))
+FROM ref_geo.l_areas
+WHERE id_type=203
+GROUP by area_code, id_area;
 
 
-;
+INSERT INTO gn_monitoring.cor_site_area (id_base_site, id_area)
+SELECT bs.id_base_site, a.id_area 
+FROM ref_geo.l_areas a
+JOIN gn_monitoring.t_base_sites bs ON ST_Within(ST_TRANSFORM(a.geom, 4326), bs.geom)
+WHERE id_type=203;
+
+-- TODO Mettre en paramètre l'id du module
+INSERT INTO gn_monitoring.cor_site_application
+SELECT  bs.id_base_site, MY_ID_MODULE
+FROM gn_monitoring.t_base_sites bs
+JOIN pr_monitoring_flora_territory.zp_tmp zp ON bs.base_site_code  = zp.id::character varying;
+
+DROP TABLE pr_monitoring_flora_territory.zp_tmp;
+DROP TABLE pr_monitoring_flora_territory.maille_tmp;
