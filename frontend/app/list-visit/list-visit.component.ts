400e7a3bbb51796db1b5a99c31cdd4118ce73c8f
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-08-14 10:58:11 +0200

    export data pour les visites d'une ZP #12

--- frontend/app/list-visit/list-visit.component.ts
+++ frontend/app/list-visit/list-visit.component.ts
@@ -15,8 +15,10 @@ import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService, ToastrConfig } from 'ngx-toastr';
 import { CommonService } from '@geonature_common/service/common.service';
 import { ModuleConfig } from '../module.config';
+import { MapListService } from '@geonature_common/map-list/map-list.service';
+
 @Component({
-    selector: 'selector-list-visit',
+    selector: 'pnx-list-visit',
     templateUrl: 'list-visit.component.html',
     styleUrls: ['./list-visit.component.scss'],
 
@@ -24,10 +26,9 @@ import { ModuleConfig } from '../module.config';
 
 export class ListVisitComponent implements OnInit, AfterViewInit {
 public zps;
+public nomTaxon; 
 public currentZp = {};
 public idSite; 
-public compteAbsent = 0; 
-public comptePresent = 0; 
 public rest; 
 public especeSuivi; 
 public infoSite; 
@@ -38,15 +39,18 @@ public visits = {};
 public codeTaxon; 
 public idVisit;
 public coordonne ; 
-
-public rows: {id_base_visit: number, visit_date: string, observers:string}[];
+public message = { emptyMessage: "Aucune visite sur ce site ", totalMessage: "visites au total" }  
+public rows = [];
 
 public columns = [
-    { name: 'Identifiant', prop:'id_base_visit' },
     { name: 'Date', prop: 'visit_date'}, 
-    { name: 'Observer', prop: "observers" },
+    { name: 'Observateur(s)', prop: "observers" },
+    { name: 'Présence/ Absence ? ', prop: "state"}
     // { name: 'Actions' }
   ];
+
+ public presence = 0; 
+ public absence = 0;  
 
 @ViewChild('geojson') geojson: GeoJsonComponent
     
@@ -59,29 +63,56 @@ public columns = [
         private _fb: FormBuilder,
         public dateParser: NgbDateParserFormatter,
         private toastr: ToastrService,
-        private _commonService: CommonService,) { }
+        private _commonService: CommonService,
+        public mapListService:  MapListService) { }
 
    ngOnInit() {
+      
       this.idSite = this.activatedRoute.snapshot.params['idSite'];
-        
-        
+      
+      this._api.getInfoSite(this.idSite).subscribe(info => { 
+         this.dataFormService.getTaxonInfo(info.cd_nom).subscribe(taxon => {
+            this.nomTaxon = taxon.nom_valide;  
+         });
+      });
+      
       const param = {
          id_base_site: this.idSite,
       }
         
       this._api.getVisits(param).subscribe(data => {
-        
+        console.log('mes data ', data );
          data.forEach( visit => {
+            
+            
             let fullName; 
             visit.observers.forEach( obs => {
                fullName = obs.nom_role + " " + obs.prenom_role; 
                 
             })
-            
             visit.observers = fullName;
+            let tabPres = [];
+            let tabAbs = [];
+            let pres = 0;
+            let abs = 0;
+            
+            visit.cor_visit_grid.forEach( maille => {
+                if (maille.presence ) {
+                    pres += 1;
+                    tabPres.push(pres);
+                } else {
+                    abs += 1; 
+                    tabAbs.push( abs);
+                }
+                   
+            });
+
+            visit.state = tabPres.length + "P / " + tabAbs.length + "A ";
+           
          });
         
-         this.rows = data;
+         
+            this.rows = data;
 
       
       })
@@ -94,6 +125,8 @@ public columns = [
       this.mapService.map.doubleClickZoom.disable();
       const parametre = {
          id_base_site: this.idSite,
+         id_application: ModuleConfig.id_application,
+
       }
       
       this.activatedRoute.params.subscribe(params => {
@@ -102,6 +135,8 @@ public columns = [
              
             this.zps = data;
             this.geojson.currentGeoJson$.subscribe(currentLayer => {
+                console.log("my currentLayer", currentLayer.getBounds());
+                
                this.mapService.map.fitBounds(currentLayer.getBounds()); 
             });
                 
@@ -118,7 +153,7 @@ public columns = [
 
    
    onEdit(id_visit) {
-      this.router.navigate([`${ModuleConfig.api_url}/editVisit`,  id_visit])
+      this.router.navigate([`${ModuleConfig.api_url}/editVisit`, this.idSite, 'visit', id_visit ])
 
    }
 
@@ -128,7 +163,15 @@ public columns = [
    }
 
    onAdd() {
-      this.router.navigate([`${ModuleConfig.api_url}/addVisit`, this.idSite]);        
+      this.router.navigate([`${ModuleConfig.api_url}/editVisit`, this.idSite]);        
    }
    
+   onDownload(format) {
+    
+        const url = `${
+          AppConfig.API_ENDPOINT}${ModuleConfig.api_url}/export_visit?id_base_site=${this.idSite}&export_format=${format}`;
+    
+        document.location.href = url;
+          
+   }
 }
