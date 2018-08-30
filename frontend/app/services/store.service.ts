a633b199b9421222dd40054464957fd4a2d696be
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-08-30 12:07:36 +0200

    ajouter les variables communes

--- frontend/app/services/store.service.ts
+++ frontend/app/services/store.service.ts
@@ -1,42 +1,44 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Layer } from 'leaflet';
-import { NgbDateParserFormatter, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AppConfig } from '@geonature_config/app.config';
+import { ModuleConfig } from '../module.config';
 
 @Injectable()
 export class StoreService {
+  public currentLayer: Layer;
 
-public currentLayer: Layer;
+  public sftConfig = ModuleConfig;
 
-
-public myStylePresent = {
-    color: "#008000",
+  public myStylePresent = {
+    color: '#008000',
     fill: true,
     fillOpacity: 0.2,
     weight: 3
-};
+  };
 
-public myStyleAbsent = {
-    color: "#8B0000",
+  public myStyleAbsent = {
+    color: '#8B0000',
     fill: true,
     fillOpacity: 0.2,
     weight: 3
-};
+  };
 
-public presence = 0;
-public absence = 0; 
-public total = 0;
-public rest = 0; 
-public exportFormat = ['geojson', 'csv', 'shapefile'];
+  public presence = 0;
+  public absence = 0;
+  public total = 0;
+  public rest = 0;
+  public urlLoad = `${AppConfig.API_ENDPOINT}${ModuleConfig.api_url}/export_visit`;
+  public queryString = new HttpParams();
 
-    
-    constructor(private _modalService: NgbModal) { } 
-   
+  constructor(private _modalService: NgbModal) {}
 
-    getMailleNoVisit() {
-        this.rest = this.total - this.absence - this.presence;
-     }
-   
-    openIntesectionModal(content) {
-        this._modalService.open(content);
-      }
+  getMailleNoVisit() {
+    this.rest = this.total - this.absence - this.presence;
+  }
+
+  openIntesectionModal(content) {
+    this._modalService.open(content);
+  }
 }
