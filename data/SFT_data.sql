90ddc45b835d21ed348c36da49636799f4ec7ba9
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-09-03 17:24:59 +0200

    up install

--- data/SFT_data.sql
+++ data/SFT_data.sql
@@ -5,7 +5,7 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
-SET search_path = pr_monitoring_flora_territory, pg_catalog;
+SET search_path = pr_monitoring_flora_territory, pg_catalog, public;
 
 
 -- créer nomenclature  ZP --  
