7f62e2f1e7a3dc2ed407a6acd0dc0da21e0505e7
Khanh-Chau <ngkhanhchau@yahoo.com>
2018-09-04 13:43:23 +0200

    update

--- frontend/app/gnModule.module.ts
+++ frontend/app/gnModule.module.ts
@@ -1,45 +1,40 @@
-import { NgModule } from "@angular/core";
-import { GN2CommonModule } from "@geonature_common/GN2Common.module";
-import { CommonModule } from "@angular/common";
-import { Routes, RouterModule } from "@angular/router";
-import { ViewTestComponent } from "./view-test/view-test.component";
-import { HttpClient } from "@angular/common/http";
-import { DataService } from "./services/data.service";
-import { HttpClientModule, HttpClientXsrfModule } from "@angular/common/http";
-import { StoreService } from "./services/store.service";
-import { DetailVisitComponent } from "./detail-visit/detail-visit.component";
-import { ZpMapListComponent } from "./zp-map-list/zp-map-list.component";
-import { ListVisitComponent } from "./list-visit/list-visit.component";
-import { FormService } from "./services/form.service";
-import { FormVisitComponent } from "./form-visit/form-visit.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientXsrfModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { GN2CommonModule } from '@geonature_common/GN2Common.module';
+
+import { DataService } from './services/data.service';
+import { StoreService } from './services/store.service';
+import { DetailVisitComponent } from './detail-visit/detail-visit.component';
+import { ZpMapListComponent } from './zp-map-list/zp-map-list.component';
+import { ListVisitComponent } from './list-visit/list-visit.component';
+import { FormService } from './services/form.service';
+import { FormVisitComponent } from './form-visit/form-visit.component';
 
 // my module routing
-const routes: Routes = 
-[{ path: "", component: ZpMapListComponent},
- { path: "listVisit/:idSite", component: ListVisitComponent},
- {path: "editVisit/:idSite", component: FormVisitComponent},
- {path: "infoVisit/:idVisit", component: DetailVisitComponent },
- {path: "editVisit/:idSite/visit/:idVisit", component: FormVisitComponent,}
-
-  
- ];
+const routes: Routes = [
+  { path: '', component: ZpMapListComponent },
+  { path: 'listVisit/:idSite', component: ListVisitComponent },
+  { path: 'editVisit/:idSite', component: FormVisitComponent },
+  { path: 'infoVisit/:idVisit', component: DetailVisitComponent },
+  { path: 'editVisit/:idSite/visit/:idVisit', component: FormVisitComponent }
+];
 
 @NgModule({
-  declarations: [ZpMapListComponent, ListVisitComponent, DetailVisitComponent, FormVisitComponent,],
+  declarations: [ZpMapListComponent, ListVisitComponent, DetailVisitComponent, FormVisitComponent],
   imports: [
     GN2CommonModule,
     HttpClientXsrfModule.withOptions({
-      cookieName: 'token',
+      git: 'token',
       headerName: 'token'
     }),
     RouterModule.forChild(routes),
     CommonModule
   ],
   providers: [HttpClient, DataService, StoreService, FormService],
-  bootstrap: [],
-  
+  bootstrap: []
 })
-
-export class GeonatureModule {
-
-}
+export class GeonatureModule {}
