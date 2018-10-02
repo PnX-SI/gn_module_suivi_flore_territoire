58eaf2969d8b22dea0047c64dc4b8f46a1e529c1
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-10-02 10:45:40 +0200

    Conf - MAJ id_list_taxon

--- config/conf_schema_toml.py
+++ config/conf_schema_toml.py
@@ -43,6 +43,6 @@ class GnModuleSchemaConf(GnModuleProdConf):
     id_type_maille = fields.Integer(missing=32)
     id_type_commune = fields.Integer(missing=25)
     id_menu_list_user = fields.Integer(missing=1)
-    id_list_taxon = fields.Integer(missing=100)
+    id_list_taxon = fields.Integer(missing=30)
     export_srid = fields.Integer(missing=2154)
     zoom_center = fields.List(fields.Float(), missing=zoom_center)
