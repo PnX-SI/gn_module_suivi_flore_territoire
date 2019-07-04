5652976d4bfd00d983fa016920bae008404ebb52
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-07-04 18:22:34 +0200

    Corrrection lié au passage a sqlachemy 1.3.3

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -4,7 +4,7 @@ import time
 
 from flask import Blueprint, request, session, current_app, send_from_directory
 from sqlalchemy.sql.expression import func
-from sqlalchemy import and_, distinct
+from sqlalchemy import and_, distinct, select
 from geojson import FeatureCollection, Feature
 from geoalchemy2.shape import to_shape
 
@@ -16,26 +16,39 @@ from geonature.core.gn_permissions.tools import get_or_fetch_user_cruved
 from geonature.utils.utilssqlalchemy import json_resp, to_json_resp, to_csv_resp
 from geonature.utils.env import DB, ROOT_DIR
 from geonature.utils.utilsgeometry import FionaShapeService
-from geonature.core.gn_monitoring.models import corVisitObserver, TBaseVisits, TBaseSites, corSiteModule, corSiteArea
+from geonature.core.gn_monitoring.models import (
+    corVisitObserver,
+    TBaseVisits,
+    TBaseSites,
+    corSiteModule,
+    corSiteArea,
+)
 from geonature.core.ref_geo.models import LAreas
 from geonature.core.users.models import BibOrganismes
 from pypnusershub.db.models import User
 
-from .models import TInfoSite, TVisiteSFT, corVisitPerturbation, CorVisitGrid, Taxonomie, ExportVisits
+from .models import (
+    TInfoSite,
+    TVisiteSFT,
+    corVisitPerturbation,
+    CorVisitGrid,
+    Taxonomie,
+    ExportVisits,
+)
 from .repositories import check_user_cruved_visit, check_year_visit
 
 
-blueprint = Blueprint('pr_suivi_flore_territoire', __name__)
+blueprint = Blueprint("pr_suivi_flore_territoire", __name__)
 
 
-@blueprint.route('/sites', methods=['GET'])
+@blueprint.route("/sites", methods=["GET"])
 @json_resp
 def get_sites_zp():
-    '''
+    """
     Retourne la liste des ZP
-    '''
+    """
     parameters = request.args
-    id_type_commune = blueprint.config['id_type_commune']
+    id_type_commune = blueprint.config["id_type_commune"]
     # grâce au fichier config
     q = (
         DB.session.query(
@@ -43,94 +56,105 @@ def get_sites_zp():
             func.max(TBaseVisits.visit_date_min),
             Taxonomie.nom_complet,
             func.count(distinct(TBaseVisits.id_base_visit)),
-            func.string_agg(distinct(BibOrganismes.nom_organisme), ', '),
-            func.string_agg(LAreas.area_name, ', ')
-        ).outerjoin(
-            TBaseVisits, TBaseVisits.id_base_site == TInfoSite.id_base_site
-        # get taxonomy lb_nom
-        ).outerjoin(
-            Taxonomie, TInfoSite.cd_nom == Taxonomie.cd_nom
-        # get organisms of a site
-        ).outerjoin(
-            corVisitObserver, corVisitObserver.c.id_base_visit == TBaseVisits.id_base_visit
-        ).outerjoin(
-            User, User.id_role == corVisitObserver.c.id_role
-        ).outerjoin(
-            BibOrganismes, BibOrganismes.id_organisme == User.id_organisme
+            func.string_agg(distinct(BibOrganismes.nom_organisme), ", "),
+            func.string_agg(LAreas.area_name, ", "),
         )
-        # get municipalities of a site
-        .outerjoin(
-            corSiteArea, corSiteArea.c.id_base_site == TInfoSite.id_base_site
-        ).outerjoin(
-            LAreas, and_(LAreas.id_area == corSiteArea.c.id_area, LAreas.id_type == id_type_commune)
+        .select_from(
+            TInfoSite.__table__.outerjoin(
+                TBaseVisits,
+                TBaseVisits.id_base_site == TInfoSite.id_base_site
+                # get taxonomy lb_nom
+            )
+            .outerjoin(
+                Taxonomie,
+                TInfoSite.cd_nom == Taxonomie.cd_nom
+                # get organisms of a site
+            )
+            .outerjoin(
+                corVisitObserver,
+                corVisitObserver.c.id_base_visit == TBaseVisits.id_base_visit,
+            )
+            .outerjoin(User, User.id_role == corVisitObserver.c.id_role)
+            .outerjoin(BibOrganismes, BibOrganismes.id_organisme == User.id_organisme)
+            # get municipalities of a site
+            .outerjoin(
+                corSiteArea, corSiteArea.c.id_base_site == TInfoSite.id_base_site
+            )
+            .outerjoin(
+                LAreas,
+                and_(
+                    LAreas.id_area == corSiteArea.c.id_area,
+                    LAreas.id_type == id_type_commune,
+                ),
+            )
         )
-        .group_by(
-            TInfoSite, Taxonomie.nom_complet
-        )
+        .group_by(TInfoSite, Taxonomie.nom_complet)
     )
 
-    if 'id_base_site' in parameters:
-        q = q.filter(TInfoSite.id_base_site == parameters['id_base_site'])
+    if "id_base_site" in parameters:
+        q = q.filter(TInfoSite.id_base_site == parameters["id_base_site"])
 
-    if 'cd_nom' in parameters:
-        q = q.filter(TInfoSite.cd_nom == parameters['cd_nom'])
-    if 'organisme' in parameters:
-        q = q.filter(BibOrganismes.nom_organisme == parameters['organisme'])
-    if 'commune' in parameters:
-        q = q.filter(LAreas.area_name == parameters['commune'])
-    if 'year' in parameters:
+    if "cd_nom" in parameters:
+        q = q.filter(TInfoSite.cd_nom == parameters["cd_nom"])
+    if "organisme" in parameters:
+        q = q.filter(BibOrganismes.nom_organisme == parameters["organisme"])
+    if "commune" in parameters:
+        q = q.filter(LAreas.area_name == parameters["commune"])
+    if "year" in parameters:
         # relance la requête pour récupérer la date_max exacte si on filtre sur l'année
         q_year = (
             DB.session.query(
-                TInfoSite.id_base_site,
-                func.max(TBaseVisits.visit_date_min),
-            ).outerjoin(
-                TBaseVisits, TBaseVisits.id_base_site == TInfoSite.id_base_site
-            ).group_by(TInfoSite.id_base_site)
+                TInfoSite.id_base_site, func.max(TBaseVisits.visit_date_min)
+            )
+            .outerjoin(TBaseVisits, TBaseVisits.id_base_site == TInfoSite.id_base_site)
+            .group_by(TInfoSite.id_base_site)
         )
 
         data_year = q_year.all()
 
-        q = q.filter(func.date_part('year', TBaseVisits.visit_date_min) == parameters['year'])
+        q = q.filter(
+            func.date_part(
+                "year", TBaseVisits.visit_date_min) == parameters["year"]
+        )
     data = q.all()
 
     features = []
     for d in data:
         feature = d[0].get_geofeature()
-        id_site = feature['properties']['base_site']['id_base_site']
-        if 'year' in parameters:
+        id_site = feature["properties"]["base_site"]["id_base_site"]
+        if "year" in parameters:
             for dy in data_year:
                 #  récupérer la bonne date max du site si on filtre sur année
                 if id_site == dy[0]:
-                    feature['properties']['date_max'] = str(dy[1])
+                    feature["properties"]["date_max"] = str(dy[1])
         else:
-            feature['properties']['date_max'] = str(d[1])
+            feature["properties"]["date_max"] = str(d[1])
             if d[1] == None:
-                feature['properties']['date_max'] = 'Aucune visite'
-        feature['properties']['nom_taxon'] = str(d[2])
-        feature['properties']['nb_visit'] = str(d[3])
-        feature['properties']['organisme'] = str(d[4])
-        feature['properties']['nom_commune'] = str(d[5])
+                feature["properties"]["date_max"] = "Aucune visite"
+        feature["properties"]["nom_taxon"] = str(d[2])
+        feature["properties"]["nb_visit"] = str(d[3])
+        feature["properties"]["organisme"] = str(d[4])
+        feature["properties"]["nom_commune"] = str(d[5])
         if d[4] == None:
-            feature['properties']['organisme'] = 'Aucun'
+            feature["properties"]["organisme"] = "Aucun"
         features.append(feature)
     return FeatureCollection(features)
 
     # return FeatureCollection([d.get_geofeature() for d in data])
 
 
-@blueprint.route('/site', methods=['GET'])
+@blueprint.route("/site", methods=["GET"])
 @json_resp
 def get_one_zp():
-    '''
+    """
     Retourne une ZP à partir de l'id_base_site
     param:
         id_base_site: integer
-    '''
+    """
     parameters = request.args
     q = DB.session.query(TInfoSite)
-    if 'id_base_site' in parameters:
-        q = q.filter(TInfoSite.id_base_site == parameters['id_base_site'])
+    if "id_base_site" in parameters:
+        q = q.filter(TInfoSite.id_base_site == parameters["id_base_site"])
     data = q.first()
 
     if data:
@@ -138,74 +162,68 @@ def get_one_zp():
     return None
 
 
-@blueprint.route('/site/<id_infos_site>', methods=['GET'])
+@blueprint.route("/site/<id_infos_site>", methods=["GET"])
 @json_resp
 def get_one_zp_id(id_infos_site):
-    '''
+    """
     Retourne une ZP à partir de l'id_info_site
-    '''
+    """
     data = DB.session.query(TInfoSite).get(id_infos_site)
     return data.as_dict()
 
 
-@blueprint.route('/visits', methods=['GET'])
+@blueprint.route("/visits", methods=["GET"])
 @json_resp
 def get_visits():
-    '''
+    """
     Retourne toutes les visites du module
-    '''
+    """
     parameters = request.args
     q = DB.session.query(TVisiteSFT)
-    if 'id_base_site' in parameters:
-        q = q.filter(TVisiteSFT.id_base_site == parameters['id_base_site'])
+    if "id_base_site" in parameters:
+        q = q.filter(TVisiteSFT.id_base_site == parameters["id_base_site"])
     data = q.all()
     return [d.as_dict(True) for d in data]
 
 
-@blueprint.route('/test', methods=['GET'])
-@json_resp
-def test():
-    data = DB.session.query(TNomenclatures).all()
-    return [d.as_dict(True) for d in data]
-
-
-@blueprint.route('/visit/<id_visit>', methods=['GET'])
+@blueprint.route("/visit/<id_visit>", methods=["GET"])
 @json_resp
 def get_visit(id_visit):
-    '''
+    """
     Retourne une visite
-    '''
+    """
     data = DB.session.query(TVisiteSFT).get(id_visit)
     return data.as_dict(recursif=True)
 
 
-@blueprint.route('/visit', methods=['POST'])
-@permissions.check_cruved_scope('R', True)
+@blueprint.route("/visit", methods=["POST"])
+@permissions.check_cruved_scope("R", True)
 @json_resp
 def post_visit(info_role):
-    '''
+    """
     Poste une nouvelle visite ou édite une ancienne
-    '''
+    """
     data = dict(request.get_json())
     # if its not an update we check if there is not aleady a visit this year
-    print('LAAAAAA')
-    print(data)
-    if not data['id_base_visit']:
-        check_year_visit(data['id_base_site'], data['visit_date_min'][0:4])
+    if not data["id_base_visit"]:
+        check_year_visit(data["id_base_site"], data["visit_date_min"][0:4])
 
     try:
-        tab_perturbation = data.pop('cor_visit_perturbation')
+        tab_perturbation = data.pop("cor_visit_perturbation")
     except:
         pass
-    tab_visit_grid = data.pop('cor_visit_grid')
-    tab_observer = data.pop('cor_visit_observer')
+    tab_visit_grid = data.pop("cor_visit_grid")
+    tab_observer = data.pop("cor_visit_observer")
     visit = TVisiteSFT(**data)
     visit.as_dict(True)
     # pour que visit prenne en compte des relations
     # sinon elle prend pas en compte le fait qu'on efface toutes les perturbations quand on édite par ex.
     try:
-        perturs = DB.session.query(TNomenclatures).filter(
-            TNomenclatures.id_nomenclature.in_(tab_perturbation)).all()
+        perturs = (
+            DB.session.query(TNomenclatures)
+            .filter(TNomenclatures.id_nomenclature.in_(tab_perturbation))
+            .all()
+        )
         for per in perturs:
             visit.cor_visit_perturbation.append(per)
     except:
@@ -214,18 +232,15 @@ def post_visit(info_role):
         visit_grid = CorVisitGrid(**v)
         visit.cor_visit_grid.append(visit_grid)
     observers = DB.session.query(User).filter(
-        User.id_role.in_(tab_observer)
-    ).all()
+        User.id_role.in_(tab_observer)).all()
     for o in observers:
         visit.observers.append(o)
 
     if visit.id_base_visit:
         user_cruved = get_or_fetch_user_cruved(
-            session=session,
-            id_role=info_role.id_role,
-            module_code='SFT'
+            session=session, id_role=info_role.id_role, module_code="SFT"
         )
-        update_cruved = user_cruved['U']
+        update_cruved = user_cruved["U"]
         check_user_cruved_visit(info_role, visit, update_cruved)
         DB.session.merge(visit)
     else:
@@ -236,81 +251,74 @@ def post_visit(info_role):
     return visit.as_dict(recursif=True)
 
 
-@blueprint.route('/export_visit', methods=['GET'])
+@blueprint.route("/export_visit", methods=["GET"])
 def export_visit():
-    '''
+    """
     Télécharge les données d'une visite (ou des visites )
-    '''
+    """
 
     parameters = request.args
     # q = q.filter(TInfoSite.id_base_site == parameters['id_base_site'])
 
-    export_format = parameters['export_format'] if 'export_format' in request.args else 'shapefile'
+    export_format = (
+        parameters["export_format"] if "export_format" in request.args else "shapefile"
+    )
 
-    file_name = datetime.datetime.now().strftime('%Y_%m_%d_%Hh%Mm%S')
-    q = (DB.session.query(ExportVisits))
+    file_name = datetime.datetime.now().strftime("%Y_%m_%d_%Hh%Mm%S")
+    q = DB.session.query(ExportVisits)
 
-    if 'id_base_visit' in parameters:
-        q = (DB.session.query(ExportVisits)
-             .filter(ExportVisits.id_base_visit == parameters['id_base_visit'])
-             )
-    elif 'id_base_site' in parameters:
-        q = (DB.session.query(ExportVisits)
-             .filter(ExportVisits.id_base_site == parameters['id_base_site'])
-             )
-    elif 'organisme' in parameters:
-        q = (DB.session.query(ExportVisits)
-             .filter(ExportVisits.organisme == parameters['organisme'])
-             )
-    elif 'commune' in parameters:
-        q = (DB.session.query(ExportVisits)
-             .filter(ExportVisits.area_name == parameters['commune'])
-             )
-    elif 'year' in parameters:
-        q = (DB.session.query(ExportVisits)
-             .filter(func.date_part('year', ExportVisits.visit_date) == parameters['year'])
-             )
-    elif 'cd_nom' in parameters:
-        q = (DB.session.query(ExportVisits)
-             .filter(ExportVisits.cd_nom == parameters['cd_nom'])
-             )
+    if "id_base_visit" in parameters:
+        q = DB.session.query(ExportVisits).filter(
+            ExportVisits.id_base_visit == parameters["id_base_visit"]
+        )
+    elif "id_base_site" in parameters:
+        q = DB.session.query(ExportVisits).filter(
+            ExportVisits.id_base_site == parameters["id_base_site"]
+        )
+    elif "organisme" in parameters:
+        q = DB.session.query(ExportVisits).filter(
+            ExportVisits.organisme == parameters["organisme"]
+        )
+    elif "commune" in parameters:
+        q = DB.session.query(ExportVisits).filter(
+            ExportVisits.area_name == parameters["commune"]
+        )
+    elif "year" in parameters:
+        q = DB.session.query(ExportVisits).filter(
+            func.date_part(
+                "year", ExportVisits.visit_date) == parameters["year"]
+        )
+    elif "cd_nom" in parameters:
+        q = DB.session.query(ExportVisits).filter(
+            ExportVisits.cd_nom == parameters["cd_nom"]
+        )
 
     data = q.all()
     features = []
 
-    if export_format == 'geojson':
+    if export_format == "geojson":
         for d in data:
-            feature = d.as_geofeature('geom', 'id_area', False)
+            feature = d.as_geofeature("geom", "id_area", False)
             features.append(feature)
         result = FeatureCollection(features)
 
-        return to_json_resp(
-            result,
-            as_file=True,
-            filename=file_name,
-            indent=4
-        )
+        return to_json_resp(result, as_file=True, filename=file_name, indent=4)
 
-    elif export_format == 'csv':
+    elif export_format == "csv":
         tab_visit = []
 
         for d in data:
             visit = d.as_dict()
             geom_wkt = to_shape(d.geom)
-            visit['geom'] = geom_wkt
+            visit["geom"] = geom_wkt
 
             tab_visit.append(visit)
 
-        return to_csv_resp(
-            file_name,
-            tab_visit,
-            tab_visit[0].keys(),
-            ';'
-        )
+        return to_csv_resp(file_name, tab_visit, tab_visit[0].keys(), ";")
 
     else:
 
-        dir_path = str(ROOT_DIR / 'backend/static/shapefiles')
+        dir_path = str(ROOT_DIR / "backend/static/shapefiles")
 
         FionaShapeService.create_shapes_struct(
             db_cols=ExportVisits.__mapper__.c,
@@ -324,57 +332,66 @@ def export_visit():
 
         FionaShapeService.save_and_zip_shapefiles()
 
-        return send_from_directory(
-            dir_path,
-            file_name+'.zip',
-            as_attachment=True
-        )
+        return send_from_directory(dir_path, file_name + ".zip", as_attachment=True)
 
 
-@blueprint.route('/commune/<id_module>', methods=['GET'])
+@blueprint.route("/commune/<id_module>", methods=["GET"])
 @json_resp
 def get_commune(id_module):
-    '''
+    """
     Retourne toutes les communes présents dans le module
-    '''
+    """
     params = request.args
 
-    q = DB.session.query(LAreas.area_name).distinct().outerjoin(
-        corSiteArea, LAreas.id_area == corSiteArea.c.id_area).outerjoin(
-        corSiteModule, corSiteModule.c.id_base_site == corSiteArea.c.id_base_site).filter(
-        corSiteModule.c.id_module == id_module)
+    q = (
+        select([LAreas.area_name.distinct()])
+        .select_from(
+            LAreas.__table__.outerjoin(
+                corSiteArea, LAreas.id_area == corSiteArea.c.id_area
+            ).outerjoin(
+                corSiteModule,
+                corSiteModule.c.id_base_site == corSiteArea.c.id_base_site,
+            )
+        )
+        .where(corSiteModule.c.id_module == id_module)
+    )
 
-    if 'id_area_type' in params:
-        q = q.filter(LAreas.id_type == params['id_area_type'])
+    if "id_area_type" in params:
+        q = q.where(LAreas.id_type == params["id_area_type"])
 
-    data = q.all()
+    data = DB.engine.execute(q)
+
     tab_commune = []
-
     for d in data:
         nom_com = dict()
-        nom_com['nom_commune'] = str(d[0])
+        nom_com["nom_commune"] = str(d[0])
         tab_commune.append(nom_com)
     return tab_commune
 
 
-@blueprint.route('/organisme', methods=['GET'])
+@blueprint.route("/organisme", methods=["GET"])
 @json_resp
 def get_organisme():
-    '''
+    """
     Retourne la liste de tous les organismes présents
-    '''
+    """
 
-    q = DB.session.query(
-        BibOrganismes.nom_organisme, User.nom_role, User.prenom_role).outerjoin(
-        User, BibOrganismes.id_organisme == User.id_organisme).distinct().join(
-        corVisitObserver, User.id_role == corVisitObserver.c.id_role).outerjoin(
-        TVisiteSFT, corVisitObserver.c.id_base_visit == TVisiteSFT.id_base_visit)
+    q = (
+        DB.session.query(BibOrganismes.nom_organisme,
+                         User.nom_role, User.prenom_role)
+        .outerjoin(User, BibOrganismes.id_organisme == User.id_organisme)
+        .distinct()
+        .join(corVisitObserver, User.id_role == corVisitObserver.c.id_role)
+        .outerjoin(
+            TVisiteSFT, corVisitObserver.c.id_base_visit == TVisiteSFT.id_base_visit
+        )
+    )
 
     data = q.all()
     tab_orga = []
     for d in data:
         info_orga = dict()
-        info_orga['nom_organisme'] = str(d[0])
-        info_orga['observer'] = str(d[1]) + ' ' + str(d[2])
+        info_orga["nom_organisme"] = str(d[0])
+        info_orga["observer"] = str(d[1]) + " " + str(d[2])
         tab_orga.append(info_orga)
     return tab_orga
--- backend/models.py
+++ backend/models.py
@@ -5,14 +5,14 @@ from geoalchemy2 import Geometry
 
 
 from geonature.utils.env import DB
-from geonature.utils.utilssqlalchemy import (
-    serializable,
-    geoserializable,
-    GenericQuery,
-)
+from geonature.utils.utilssqlalchemy import serializable, geoserializable, GenericQuery
 from geonature.utils.utilsgeometry import shapeserializable
 from geonature.core.gn_synthese.models import synthese_export_serialization
-from geonature.core.gn_monitoring.models import TBaseSites, TBaseVisits, corVisitObserver
+from geonature.core.gn_monitoring.models import (
+    TBaseSites,
+    TBaseVisits,
+    corVisitObserver,
+)
 from geonature.core.ref_geo.models import LAreas
 from pypnnomenclature.models import TNomenclatures
 from pypnusershub.db.models import User
@@ -21,69 +21,53 @@ from pypnusershub.db.models import User
 @serializable
 @geoserializable
 class TInfoSite(DB.Model):
-    '''
+    """
     Modèle d'une ZP
-    '''
-    __tablename__ = 't_infos_site'
-    __table_args__ = {'schema': 'pr_monitoring_flora_territory'}
+    """
+
+    __tablename__ = "t_infos_site"
+    __table_args__ = {"schema": "pr_monitoring_flora_territory"}
 
     id_infos_site = DB.Column(DB.Integer, primary_key=True)
     # fk gn_monitoring.base_site
-    id_base_site = DB.Column(
-        DB.Integer,
-        ForeignKey(TBaseSites.id_base_site)
-    )
+    id_base_site = DB.Column(DB.Integer, ForeignKey(TBaseSites.id_base_site))
     base_site = DB.relationship(TBaseSites)
-    geom = association_proxy('base_site', 'geom')
+    geom = association_proxy("base_site", "geom")
     cd_nom = DB.Column(DB.Integer)
 
     def get_geofeature(self, recursif=True):
-        return self.as_geofeature(
-            'geom',
-            'id_infos_site',
-            recursif
-        )
+        return self.as_geofeature("geom", "id_infos_site", recursif)
 
 
-'''
-Corespondance entre une maille et une visite
-'''
-corVisitPerturbation = DB.Table(
-    'cor_visit_perturbation',
-    DB.MetaData(schema='pr_monitoring_flora_territory'),
-    DB.Column(
-        'id_base_visit',
+class corVisitPerturbation(DB.Model):
+    __tablename__ = "cor_visit_perturbation"
+    __table_args__ = {"schema": "pr_monitoring_flora_territory"}
+    id_base_visit = DB.Column(
+        "id_base_visit",
         DB.Integer,
-        ForeignKey(TBaseVisits.id_base_visit),
+        ForeignKey("gn_monitoring.t_base_visits.id_base_visit"),
         primary_key=True,
-    ),
-    DB.Column(
-        'id_nomenclature_perturbation',
+    )
+    id_nomenclature_perturbation = DB.Column(
+        "id_nomenclature_perturbation",
         DB.Integer,
         ForeignKey(TNomenclatures.id_nomenclature),
-        primary_key=True
+        primary_key=True,
     )
-)
 
 
 @serializable
-# @geoserializable
 class CorVisitGrid(DB.Model):
-    '''
+    """
     Corespondance entre une maille et une visite
-    '''
-    __tablename__ = 'cor_visit_grid'
-    __table_args__ = {'schema': 'pr_monitoring_flora_territory'}
+    """
 
-    id_area = DB.Column(
-        DB.Integer,
-        ForeignKey(LAreas.id_area),
-        primary_key=True
-    )
+    __tablename__ = "cor_visit_grid"
+    __table_args__ = {"schema": "pr_monitoring_flora_territory"}
+
+    id_area = DB.Column(DB.Integer, ForeignKey(LAreas.id_area), primary_key=True)
     id_base_visit = DB.Column(
-        DB.Integer,
-        ForeignKey(TBaseVisits.id_base_visit),
-        primary_key=True
+        DB.Integer, ForeignKey(TBaseVisits.id_base_visit), primary_key=True
     )
     presence = DB.Column(DB.Boolean)
     uuid_base_visit = DB.Column(UUID(as_uuid=True))
@@ -92,64 +76,48 @@ class CorVisitGrid(DB.Model):
 @serializable
 @shapeserializable
 class TVisiteSFT(TBaseVisits):
-    '''
+    """
     Visite sur une ZP
     et corespondance avec ses mailles
-    '''
-    __tablename__ = 't_base_visits'
-    __table_args__ = {
-        'schema': 'gn_monitoring',
-        'extend_existing': True
-    }
+    """
+
+    __tablename__ = "t_base_visits"
+    __table_args__ = {"schema": "gn_monitoring", "extend_existing": True}
 
     cor_visit_grid = DB.relationship(
-        'CorVisitGrid',
-        primaryjoin=(
-            CorVisitGrid.id_base_visit == TBaseVisits.id_base_visit
-        ),
-        foreign_keys=[
-            CorVisitGrid.id_base_visit,
-        ]
+        "CorVisitGrid",
+        primaryjoin=(CorVisitGrid.id_base_visit == TBaseVisits.id_base_visit),
+        foreign_keys=[CorVisitGrid.id_base_visit],
     )
     cor_visit_perturbation = DB.relationship(
         TNomenclatures,
-        secondary=corVisitPerturbation,
-        primaryjoin=(
-            corVisitPerturbation.c.id_base_visit == TBaseVisits.id_base_visit
+        secondary=corVisitPerturbation.__table__,
+        primaryjoin=(corVisitPerturbation.id_base_visit == TBaseVisits.id_base_visit),
+        secondaryjoin=(
+            corVisitPerturbation.id_nomenclature_perturbation
+            == TNomenclatures.id_nomenclature
         ),
-        secondaryjoin=(corVisitPerturbation.c.id_nomenclature_perturbation == TNomenclatures.id_nomenclature),
         foreign_keys=[
-            corVisitPerturbation.c.id_base_visit,
-            corVisitPerturbation.c.id_nomenclature_perturbation,
-        ]
+            corVisitPerturbation.id_base_visit,
+            corVisitPerturbation.id_nomenclature_perturbation,
+        ],
     )
 
     observers = DB.relationship(
-        'User',
+        "User",
         secondary=corVisitObserver,
-        primaryjoin=(
-            corVisitObserver.c.id_base_visit == TBaseVisits.id_base_visit
-        ),
+        primaryjoin=(corVisitObserver.c.id_base_visit == TBaseVisits.id_base_visit),
         secondaryjoin=(corVisitObserver.c.id_role == User.id_role),
-        foreign_keys=[
-            corVisitObserver.c.id_base_visit,
-            corVisitObserver.c.id_role,
-        ]
+        foreign_keys=[corVisitObserver.c.id_base_visit, corVisitObserver.c.id_role],
     )
 
 
 @serializable
 class Taxonomie(DB.Model):
-    __tablename__ = 'taxref'
-    __table_args__ = {
-        'schema': 'taxonomie',
-        'extend_existing': True
-    }
+    __tablename__ = "taxref"
+    __table_args__ = {"schema": "taxonomie", "extend_existing": True}
 
-    cd_nom = DB.Column(
-        DB.Integer,
-        primary_key=True
-    )
+    cd_nom = DB.Column(DB.Integer, primary_key=True)
     nom_complet = DB.Column(DB.Unicode)
 
 
@@ -157,23 +125,15 @@ class Taxonomie(DB.Model):
 @geoserializable
 @shapeserializable
 class ExportVisits(DB.Model):
-    __tablename__ = 'export_visits'
-    __table_args__ = {
-        'schema': 'pr_monitoring_flora_territory',
-    }
-    id_area = DB.Column(
-        DB.Integer,
-        primary_key=True
-    )
-    id_base_visit = DB.Column(
-        DB.Integer,
-        primary_key=True
-    )
+    __tablename__ = "export_visits"
+    __table_args__ = {"schema": "pr_monitoring_flora_territory"}
+    id_area = DB.Column(DB.Integer, primary_key=True)
+    id_base_visit = DB.Column(DB.Integer, primary_key=True)
     id_base_site = DB.Column(DB.Integer)
     uuid_base_visit = DB.Column(UUID(as_uuid=True))
     visit_date_min = DB.Column(DB.DateTime)
     comments = DB.Column(DB.Unicode)
-    geom = DB.Column(Geometry('GEOMETRY', 2154))
+    geom = DB.Column(Geometry("GEOMETRY", 2154))
     presence = DB.Column(DB.Boolean)
     label_perturbation = DB.Column(DB.Unicode)
     observateurs = DB.Column(DB.Unicode)
