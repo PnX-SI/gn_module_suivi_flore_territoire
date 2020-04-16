244dd095043e07d9dc58b2788ffd968ac1221d98
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2020-04-16 11:18:36 +0200

    Fix import_nomenclature: handle broaders deleting

--- bin/import_nomenclatures.sh
+++ bin/import_nomenclatures.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 # DESC: Usage help
 # ARGS: None
 # OUTS: None
-function printScriptUsage() { 
+function printScriptUsage() {
     cat << EOF
 Usage: ./import_taxons.sh [options]
 Update settings.ini, section "Import nomenclatures" before run this script.
@@ -75,16 +75,14 @@ function main() {
     #+----------------------------------------------------------------------------------------------------------+
     # Start script
     printInfo "Nomenclatures import script started at: ${fmt_time_start}"
-    
+
     # Manage verbosity
-    if [[ -n ${verbose-} ]]; then 
+    if [[ -n ${verbose-} ]]; then
         readonly psql_verbosity="${psql_verbose_opts-}"
     else
         readonly psql_verbosity="${psql_quiet_opts-}"
-        readonly tasks_count="$(($(csvtool height "${nomenclatures_csv_path}") - 1))"
-        tasks_done=0
     fi
-    
+
     if [[ "$action" = "import" ]]; then
         importNomenclatures
     elif [[ "$action" = "delete" ]]; then
@@ -99,62 +97,95 @@ function main() {
 function importNomenclatures() {
     local head="$(csvtool head 1 "${nomenclatures_csv_path}")"
     printMsg "Import nomenclatures list into « ref_nomenclatures.t_nomenclatures »"
-    stdbuf -oL csvtool drop 1 "${nomenclatures_csv_path}"  |
-        while IFS= read -r line; do 
-            local type_code="$(printf "$head\n$line" | csvtool namedcol type_nomenclature_code - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local code="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local mnemonique="$(printf "$head\n$line" | csvtool namedcol mnemonique - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local label="$(printf "$head\n$line" | csvtool namedcol label_default - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local definition="$(printf "$head\n$line" | csvtool namedcol definition_default - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local label_fr="$(printf "$head\n$line" | csvtool namedcol label_fr - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local definition_fr="$(printf "$head\n$line" | csvtool namedcol definition_fr - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local broader="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature_broader - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local hierarchy="$(printf "$head\n$line" | csvtool namedcol hierarchy - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            
-            printVerbose "Inserting nomenclature: '${code}' from '${type_code}'"
-            export PGPASSWORD="${user_pg_pass}"; \
-                psql -h "${db_host}" -U "${user_pg}" -d "${db_name}" ${psql_verbosity} \
-                    -v typeCode="${type_code}" \
-                    -v code="${code}" \
-                    -v mnemonique="${mnemonique}" \
-                    -v label="${label}" \
-                    -v labelFr="${label_fr}" \
-                    -v definition="${definition}" \
-                    -v definitionFr="${definition_fr}" \
-                    -v broader="${broader}" \
-                    -v hierarchy="${hierarchy}" \
-                    -f "${data_dir}/import_nomenclatures.sql"
+    local tasks_done=0
+    local tasks_count="$(($(csvtool height "${nomenclatures_csv_path}") - 1))"
+    while IFS= read -r line; do
+        local type_code="$(printf "$head\n$line" | csvtool namedcol type_nomenclature_code - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local code="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local mnemonique="$(printf "$head\n$line" | csvtool namedcol mnemonique - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local label="$(printf "$head\n$line" | csvtool namedcol label_default - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local definition="$(printf "$head\n$line" | csvtool namedcol definition_default - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local label_fr="$(printf "$head\n$line" | csvtool namedcol label_fr - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local definition_fr="$(printf "$head\n$line" | csvtool namedcol definition_fr - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local broader="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature_broader - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local hierarchy="$(printf "$head\n$line" | csvtool namedcol hierarchy - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
 
-            if ! [[ -n ${verbose-} ]]; then 
-                (( tasks_done += 1 ))
-                displayProgressBar $tasks_count $tasks_done "inserting"
-            fi
-        done
+        printVerbose "Inserting nomenclature: '${code}' from '${type_code}'"
+        export PGPASSWORD="${user_pg_pass}"; \
+            psql -h "${db_host}" -U "${user_pg}" -d "${db_name}" ${psql_verbosity} \
+                -v typeCode="${type_code}" \
+                -v code="${code}" \
+                -v mnemonique="${mnemonique}" \
+                -v label="${label}" \
+                -v labelFr="${label_fr}" \
+                -v definition="${definition}" \
+                -v definitionFr="${definition_fr}" \
+                -v broader="${broader}" \
+                -v hierarchy="${hierarchy}" \
+                -f "${data_dir}/import_nomenclatures.sql"
+
+        if ! [[ -n ${verbose-} ]]; then
+            (( tasks_done += 1 ))
+            displayProgressBar $tasks_count $tasks_done "inserting"
+        fi
+    done < <(stdbuf -oL csvtool drop 1 "${nomenclatures_csv_path}")
     echo
 }
 
 function deleteNomenclatures() {
-    printMsg "Delete nomenclatures listed in CSV file from « ref_nomenclatures.t_nomenclatures »"
+    printMsg "Delete child nomenclatures listed in CSV file from « ref_nomenclatures.t_nomenclatures »"
+    parents=()
+    deleteChildNomenclatures
+    deleteParentNomenclatures
+}
 
+function deleteChildNomenclatures() {
     local head="$(csvtool head 1 "${nomenclatures_csv_path}")"
-    stdbuf -oL csvtool drop 1 "${nomenclatures_csv_path}"  |
-        while IFS= read -r line; do
-            local type_code="$(printf "$head\n$line" | csvtool namedcol type_nomenclature_code - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
-            local code="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+    local tasks_done=0
+    local tasks_count="$(($(csvtool height "${nomenclatures_csv_path}") - 1))"
+    while IFS= read -r line; do
+        local type_code="$(printf "$head\n$line" | csvtool namedcol type_nomenclature_code - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local code="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
+        local broader="$(printf "$head\n$line" | csvtool namedcol cd_nomenclature_broader - | sed 1d | sed -e 's/^"//' -e 's/"$//')"
 
-            printVerbose "Deleting nomenclature: '${code}' from '${type_code}'"
+        if [[ "${broader}" == "0" ]]; then
+            parents+=("${type_code}|${code}")
+        else
+            printVerbose "Deleting child nomenclatures: '${code}' from '${type_code}'"
             export PGPASSWORD="${user_pg_pass}"; \
+                psql -h "${db_host}" -U "${user_pg}" -d "${db_name}" ${psql_verbosity} \
+                    -v typeCode="${type_code}" \
+                    -v code="${code}" \
+                    -f "${data_dir}/delete_nomenclatures.sql"
+        fi
+
+        if ! [[ -n ${verbose-} ]]; then
+            (( tasks_done += 1 ))
+            displayProgressBar $tasks_count $tasks_done "deleting childs"
+        fi
+    done < <(stdbuf -oL csvtool drop 1 "${nomenclatures_csv_path}")
+    echo
+}
+
+function deleteParentNomenclatures() {
+    printMsg "Delete parent nomenclatures listed in CSV file from « ref_nomenclatures.t_nomenclatures »"
+    local tasks_done=0
+    local tasks_count="${#parents[@]}"
+    for data in "${parents[@]}";do
+        local type_code="${data%|*}"
+        local code="${data#*|}"
+        printVerbose "Deleting parent nomenclatures: '${code}' from '${type_code}'"
+        export PGPASSWORD="${user_pg_pass}"; \
             psql -h "${db_host}" -U "${user_pg}" -d "${db_name}" ${psql_verbosity} \
                 -v typeCode="${type_code}" \
                 -v code="${code}" \
                 -f "${data_dir}/delete_nomenclatures.sql"
-            
-            if ! [[ -n ${verbose-} ]]; then 
-                (( tasks_done += 1 ))
-                displayProgressBar $tasks_count $tasks_done "deleting"
-            fi
-        done
-    echo
+
+        if ! [[ -n ${verbose-} ]]; then
+            (( tasks_done += 1 ))
+            displayProgressBar $tasks_count $tasks_done "deleting parents"
+        fi
+    done
 }
 
 main "${@}"
